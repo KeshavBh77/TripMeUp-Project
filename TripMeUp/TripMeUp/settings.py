2f208a854e670f92728909b26fa662457917aca4
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-04-14 04:16:41 -0600

    Minor change

--- TripMeUp/TripMeUp/settings.py
+++ TripMeUp/TripMeUp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TripMeUpApp.apps.AppConfig',
+    'TripMeUpApp.apps.TripmeupappConfig',
 ]
 
 MIDDLEWARE = [
