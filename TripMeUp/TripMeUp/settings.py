af3f9759f7e497b60ac6723585ad7a2dcfc49bd5
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-04-14 04:36:30 -0600

    changes

--- TripMeUp/TripMeUp/settings.py
+++ TripMeUp/TripMeUp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TripMeUpApp.apps.TripmeupappConfig',
+    'TripMeUpApp.apps.TripMeUpAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'TripMeUp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Keshav71$default',
+        'USER':'Keshav71',
+        'PASSWORD':'data1234',
+        'HOST':'Keshav71.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
     }
 }
 
