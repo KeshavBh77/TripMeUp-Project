f7569ae4e6b25946ba31e8f86cf6572b8c966472
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-04-15 18:49:41 -0600

    changes

--- TripMeUp/TripMeUpApp/views.py
+++ TripMeUp/TripMeUpApp/views.py
@@ -1,8 +1,16 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import City
+from rest_framework.views import APIView, HttpResponseBase
+from .serializers import CitySerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
-def home(request):
-   return HttpResponse("Hello world")
+class CityListView(APIView):
+   def get(self, request):
+      cities = City.objects.all().values()
+      serializer = CitySerializer(cities, many=True)
+      return Response("Hello")
+
