39d90ba2270ae9316fc0dacc7c956f742d4d2185
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-04-18 14:15:25 -0600

    changes

--- TripMeUp/TripMeUpApp/admin.py
+++ TripMeUp/TripMeUpApp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import City
+from .models import *
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Writes)
+class WritesAdmin(admin.ModelAdmin):
+    pass
+@admin.register(ClientCuisine)
+class CCAdmin(admin.ModelAdmin):
     pass
--- TripMeUp/TripMeUpApp/views.py
+++ TripMeUp/TripMeUpApp/views.py
@@ -5,11 +5,11 @@ from .models import City
 from rest_framework.views import APIView, HttpResponseBase
 from .serializers import CitySerializer
 from rest_framework.response import Response
-
+from rest_framework.generics import ListAPIView
 # Create your views here.
 
-class CityListView(APIView):
-   def get(self, request):
+class CityListView(ListAPIView):
+   def get(self, request, **kwargs):
       cities = City.objects.all().values()
       serializer = CitySerializer(cities, many=True)
       return Response(cities)
